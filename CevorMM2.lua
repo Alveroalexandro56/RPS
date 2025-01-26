c759050fd919204efbed3254499aa6e7f0f6b093
Alveroalexandro56 <Worldroleplayman@gmail.com>
2025-01-26 16:15:01 -0500

    CevorMM2.lua

--- CevorMM2.lua
+++ CevorMM2.lua
@@ -1,6 +1,6 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Alveroalexandro56/RPS/refs/heads/main/Hihlightplayers.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2"))()
 loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
 Print("Cevor MM2>>>")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Alveroalexandro56/RPS/refs/heads/main/Hihlightplayers.lua"))()
