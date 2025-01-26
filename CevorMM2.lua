a61831ae9aac64fc6f22fff0d0a8e47b02113c37
Alveroalexandro56 <Worldroleplayman@gmail.com>
2025-01-26 16:59:13 -0500

    CevorMM2.lua

--- CevorMM2.lua
+++ CevorMM2.lua
@@ -2,7 +2,7 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 
 -- Load and execute second script
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", true))()
 
 -- Load and execute third script
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2"))()
