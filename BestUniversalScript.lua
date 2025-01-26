42c438931ed668df1abd2480670e004a2dfc59bd
Alveroalexandro56 <Worldroleplayman@gmail.com>
2025-01-26 15:55:03 -0500

    CevorMM2.lua

--- BestUniversalScript.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Alveroalexandro56/RPS/refs/heads/main/Hihlightplayers.lua"))()
