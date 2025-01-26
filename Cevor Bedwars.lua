a28153cb8f8efc3f26dcfe14ed6cb2f83b5c1e5d
Alveroalexandro56 <Worldroleplayman@gmail.com>
2025-01-26 17:16:27 -0500

    Cevor Bedwars.lua

--- Cevor Bedwars.lua
+++ Cevor Bedwars.lua
@@ -1,3 +1,5 @@
 -- RIGHT SHIFT TO OPEN GUI
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/vapevoidware/main/NewMainScript.lua", true))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
