19327754d1b6eb058229e70dadf59877445ee485
Alveroalexandro56 <Worldroleplayman@gmail.com>
2025-01-26 16:47:11 -0500

    CevorMM2IsBestYeah.lua

--- CevorMM2IsBestYeah.lua
+++ CevorMM2IsBestYeah.lua
@@ -1,2 +1,4 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Alveroalexandro56/RPS/refs/heads/main/CevorMM2.lua'))()
-
+loadstring(
+  game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua")
+)()
