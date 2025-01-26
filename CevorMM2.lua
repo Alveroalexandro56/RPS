b30224d47199cd5274035dcab38ab15bcc961b0a
Alveroalexandro56 <Worldroleplayman@gmail.com>
2025-01-26 16:30:51 -0500

    CevorMM2.lua

--- CevorMM2.lua
+++ CevorMM2.lua
@@ -1,5 +1,17 @@
+-- Load and execute first script
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+
+-- Load and execute second script
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
+
+-- Load and execute third script
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Au0yX/Community/main/XhubMM2"))()
+
+-- Load and execute fourth script
 loadstring(game:HttpGet('https://pastebin.com/raw/3Rnd9rHf'))()
-Print("Cevor MM2>>>")
+
+-- Load and execute fifth script
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Alveroalexandro56/RPS/refs/heads/main/Hihlightplayers.lua'))()
+
+-- Output a message after executing all scripts
+print("Cevor MM2>>>")
