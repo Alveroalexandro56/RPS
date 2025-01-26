a721ae6c0b669d99ab780e2f6eb597d5d5504a86
Alveroalexandro56 <Worldroleplayman@gmail.com>
2025-01-26 16:23:32 -0500

    CevorMM2IsBestYeah.lua

--- CevorMM2IsBest.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Alveroalexandro56/RPS/refs/heads/main/CevorMM2.lua"))()
---
+++ CevorMM2IsBestYeah.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Alveroalexandro56/RPS/refs/heads/main/CevorMM2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Alveroalexandro56/RPS/refs/heads/main/Hihlightplayers.lua"))()
